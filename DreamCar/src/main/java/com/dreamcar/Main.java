878a51c49d382cd12af1013ee2d89e8077cf7b8c
Abdulkadir Yanar <abdulkadiryanar03@gmail.com>
2025-05-12 10:59:08 -0700

    Inheritance from Super Class
    init commit

--- DreamCar/src/main/java/com/dreamcar/Main.java
+++ DreamCar/src/main/java/com/dreamcar/Main.java
@@ -1,7 +1,20 @@
 package com.dreamcar;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        File file = new File("src/main/resources/data/inventory.csv");
+        if (file.exists()) {
+            System.out.println("CSV file found: " + file.getAbsolutePath());
+        } else {
+            System.out.println("CSV file not found!");
+        }
+        try {
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- DreamCar/src/main/java/com/dreamcar/data/DealershipFileManager.java
+++ DreamCar/src/main/java/com/dreamcar/data/DealershipFileManager.java
@@ -1,4 +1,23 @@
 package com.dreamcar.data;
 
+import com.dreamcar.model.Dealership;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.Buffer;
+
 public class DealershipFileManager {
+    private final String FILE_NAME = "inventory.csv";
+
+    public Dealership getDealership() {
+       Dealership dealership=null;
+    try {BufferedReader br =new BufferedReader(new FileReader(FILE_NAME)){
+        String line = br.readLine();
+    }
+
+    } catch (Exception e) {
+        throw new RuntimeException(e);
+    }
+    return dealership;
+    }
 }
